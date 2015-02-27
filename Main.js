f17cff26c8fb8b9103caf134c406b013b56d9315
Olle Pejstrup <pejstrup@hotmail.se>
2015-02-27 23:21:41 +0100

    Cool useless client

--- Main.js
+++ Main.js
@@ -1,1 +1,9 @@
-  var socket = io("http://91.123.198.103:8080");
+  //var socket = io("http://91.123.198.103:8080");
+  
+ /* socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+  
+     socket.on("derp", function(object){
+       alert(object);
+   });*/
---
+++ dataFromServer.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+  var socket = io("http://91.123.198.103:8080");
+  
+  socket.on("gameState", function(data){
+  console.log(data);
+  
+  paintRec(data);
+  
+ // var 
+  });
+  
+ /* socket.emit('ballRequest', 'Please I want to play, senpai');
+  
+  
+   socket.on("EnFinBoll", function(object){
+       console.log(object);
+   });
+
+
+   
+     socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+   
+  });*/
+  
+
