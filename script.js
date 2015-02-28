effde2b46709e30705fa44a8eabc4c085edb09fc
antonoden <antonoden@hotmail.com>
2015-02-28 17:43:55 +0100

    moved draFunctions to own file. To get some orientation in script.js

--- script.js
+++ script.js
@@ -23,36 +23,6 @@ function getInfo() {
     console.log(jsonObj);
 }
 /*****************************************************************************/
-function drawMap(ctx) {
-    if(mapObj) {
-        mapObj.forEach(function(obj) {
-            ctx.fillStyle = obj.color;
-            if(obj.type === "triangle") 
-            {
-                ctx.beginPath();
-                ctx.moveTo(obj.x, obj.y);
-                ctx.lineTo(obj.x+10, obj.y-10);
-                ctx.lineTo(obj.x-10, obj.y-10);
-                ctx.closePath();
-                ctx.fill();
-            }
-            else if(obj.type === "circle") 
-            {
-                ctx.beginPath();
-                ctx.arc(obj.x, obj.y, obj.size.radius, 0, 2*Math.PI);
-                ctx.closePath();
-                ctx.lineWidth = 0;
-                ctx.stroke();
-                ctx.fill();
-            }
-            else // default type = rectangle
-            {
-                ctx.fillRect(obj.x, obj.y, obj.size.w, obj.size.h);
-            }
-            
-        });
-    }
-}
 
 function setRandomPlayerName() {
     
@@ -100,6 +70,12 @@ function setPlayerName() {
     socket.emit("updatePlayerName", playerName);
 }
 
+function updateServer(input) 
+{
+    socket.emit("update", input);
+    input.shoot = 0;
+}
+
 function updateCanvasLoop() {
     
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -111,78 +87,6 @@ function updateCanvasLoop() {
     updateServer(input);
     
     window.requestAnimationFrame(updateCanvasLoop);
-}
-
-function updateServer(input) 
-{
-    socket.emit("update", input);
-    input.shoot = 0;
-}
-
-function drawPlayers(ctx)
-{
-    if(playersObj) 
-    {
-        playersObj.forEach(function(playerobj){
-            drawHead(ctx, playerobj);
-            drawWeapon(ctx, playerobj);
-            drawPlayerName(ctx, playerobj);
-        });
-    }
-}
-
-function drawPlayerName(ctx, playerobj) {
-    ctx.font = "20px Georgia";
-    ctx.fillText(
-            playerobj.displayName, 
-            playerobj.x-(playerobj.radius/2), 
-            playerobj.y-playerobj.radius-20);
-}
-
-function drawHead(ctx, playerobj)
-{
-    ctx.fillStyle = playerobj.color;
-    ctx.beginPath();
-    ctx.arc(playerobj.x, playerobj.y, playerobj.radius, 0, 2*Math.PI);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.fill();
-    ctx.fillStyle = "#000000";
-    ctx.beginPath();
-    ctx.arc(playerobj.x, playerobj.y, playerobj.radius/4, 0, 2*Math.PI);
-    ctx.closePath();
-    ctx.lineWidth = 10;
-    ctx.stroke();
-    ctx.fill();
-}
-
-function drawWeapon(ctx, playerobj)
-{
-    pistolSize = 75;
-    ctx.fillStyle = "#000000";
-    ctx.beginPath();
-    ctx.moveTo(playerobj.x,playerobj.y);
-    ctx.lineTo(playerobj.x + pistolSize*Math.cos(playerobj.rotation)
-            ,playerobj.y + pistolSize*Math.sin(playerobj.rotation));
-    ctx.lineWidth = 10;
-    ctx.stroke();
-}
-
-function drawProjectiles(ctx)
-{
-    if(jsonObj)
-    {
-        jsonObj.projectiles.forEach(function(projectile)
-        {
-                ctx.fillStyle="#FF0000";
-                ctx.beginPath();
-                ctx.arc(projectile.x, projectile.y, projectile.radius, 0, 2*Math.PI);
-                ctx.closePath();
-                ctx.lineWidth = 15;
-                ctx.stroke();
-                ctx.fill();
-        });
-    }
 }
 
 function setWeaponSound(sound)
